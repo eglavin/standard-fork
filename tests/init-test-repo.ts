d45496aca06c2f6ffe440c469efc98990998c71c
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2023-10-27 23:55:21 +0100

    test: add temp test folder scripts

---
+++ tests/init-test-repo.ts
@@ -0,0 +1,66 @@
+import { join } from "node:path";
+import { existsSync, mkdirSync, rmSync, writeFileSync } from "node:fs";
+import { ExecSyncOptionsWithBufferEncoding, execSync } from "node:child_process";
+
+export function createTestFolder() {
+	const tempDirLocation = join(process.cwd(), "..", ".temp");
+	const tempFolderName = `fork-version-${Date.now()}`;
+	const tempDir = join(tempDirLocation, tempFolderName);
+
+	const execSyncOptions: ExecSyncOptionsWithBufferEncoding = {
+		cwd: tempDir,
+		stdio: "ignore",
+	};
+
+	if (!existsSync(tempDir)) {
+		mkdirSync(tempDir, { recursive: true });
+
+		if (!existsSync(tempDir)) throw new Error("Unable to create test folder.");
+	} else throw new Error("Test folder already exists.");
+
+	function setupGitRepo() {
+		execSync("git init", execSyncOptions);
+		execSync("git config commit.gpgSign false", execSyncOptions);
+		execSync("git config core.autocrlf false", execSyncOptions);
+	}
+
+	function createPackageJson(packageJson?: object) {
+		const stringifiedPackage = JSON.stringify(packageJson || { version: "1.0.0" }, null, 2);
+
+		writeFileSync(join(tempDir, "package.json"), stringifiedPackage, "utf-8");
+		execSync("git add package.json", execSyncOptions);
+	}
+
+	function createCommits(commits?: string[]) {
+		const TestCommits = Array.isArray(commits)
+			? commits
+			: [
+					"initial commit",
+					"feat: A feature commit.",
+					"perf: A performance change.",
+					"chore: A chore commit.",
+					"ci: A ci commit.",
+					"custom: A custom commit.",
+			  ];
+
+		for (let index = 0; index < TestCommits.length; index++) {
+			const commitMessage = TestCommits[index];
+
+			execSync(`git commit --allow-empty -m "${commitMessage}"`, execSyncOptions);
+		}
+	}
+
+	function deleteTempFolder() {
+		rmSync(tempDir, { recursive: true, force: true });
+	}
+
+	return {
+		tempFolderName,
+		tempDir,
+
+		setupGitRepo,
+		createPackageJson,
+		createCommits,
+		deleteTempFolder,
+	};
+}
