e7f55392a4bc8109f2636e4b7b757dd6a29d67b9
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2024-04-06 23:26:06 +0100

    refactor: modify commit logic to ensure changelog exists and commit all works

---
+++ src/process/__tests__/commit.test.ts
@@ -0,0 +1,92 @@
+import { writeFileSync } from "node:fs";
+import { join } from "node:path";
+import { execFile } from "node:child_process";
+
+import { createTestDir } from "../../../tests/create-test-directory";
+import { commitChanges } from "../commit";
+
+describe("commit", () => {
+	it("should commit changed files", async () => {
+		const { testDir, deleteTestDir, createTestConfig } = createTestDir("changelog");
+
+		const { config, logger } = await createTestConfig();
+
+		writeFileSync(join(testDir, "CHANGELOG.md"), "Test content");
+		writeFileSync(join(testDir, "package.json"), JSON.stringify({ version: "1.2.3" }));
+		writeFileSync(join(testDir, "README.md"), "Test readme content");
+
+		await commitChanges(
+			config,
+			logger,
+			[
+				{ name: "CHANGELOG.md", path: join(testDir, "CHANGELOG.md"), version: "1.2.4" },
+				{ name: "package.json", path: join(testDir, "package.json"), version: "1.2.4" },
+			],
+			"1.2.4",
+		);
+
+		await execFile(
+			"git",
+			["status", "--porcelain"],
+			{
+				cwd: testDir,
+			},
+			(error, stdout) => expect(stdout).toContain("?? README.md"),
+		);
+
+		deleteTestDir();
+	});
+
+	it("should not commit if there are no files to commit", async () => {
+		const { testDir, deleteTestDir, createTestConfig } = createTestDir("changelog");
+
+		const { config, logger } = await createTestConfig();
+
+		writeFileSync(join(testDir, "README.md"), "Test readme content");
+
+		await commitChanges(config, logger, [], "1.2.4");
+
+		await execFile(
+			"git",
+			["status", "--porcelain"],
+			{
+				cwd: testDir,
+			},
+			(error, stdout) => expect(stdout).toContain(""),
+		);
+
+		deleteTestDir();
+	});
+
+	it("should commit all files if commitAll is set to true", async () => {
+		const { testDir, deleteTestDir, createTestConfig } = createTestDir("changelog");
+
+		const { config, logger } = await createTestConfig();
+		config.commitAll = true;
+
+		writeFileSync(join(testDir, "CHANGELOG.md"), "Test content");
+		writeFileSync(join(testDir, "package.json"), JSON.stringify({ version: "1.2.3" }));
+		writeFileSync(join(testDir, "README.md"), "Test readme content");
+
+		await commitChanges(
+			config,
+			logger,
+			[
+				{ name: "CHANGELOG.md", path: join(testDir, "CHANGELOG.md"), version: "1.2.4" },
+				{ name: "package.json", path: join(testDir, "package.json"), version: "1.2.4" },
+			],
+			"1.2.4",
+		);
+
+		await execFile(
+			"git",
+			["status", "--porcelain"],
+			{
+				cwd: testDir,
+			},
+			(error, stdout) => expect(stdout).toBe(""),
+		);
+
+		deleteTestDir();
+	});
+});
--- src/process/commit.ts
+++ src/process/commit.ts
@@ -1,5 +1,7 @@
+import { resolve } from "node:path";
 import { createExecute } from "../utils/execute-file";
 import { formatCommitMessage } from "../utils/format-commit-message";
+import { fileExists } from "../utils/file-state";
 import type { ForkConfig } from "../config/schema";
 import type { FileState } from "../strategies/file-manager";
 import type { Logger } from "../utils/logger";
@@ -20,9 +22,12 @@ export async function commitChanges(
 
 	logger.log("Committing changes");
 
-	const filesToCommit: string[] = [config.changelog];
+	const filesToCommit: string[] = [];
+	if (fileExists(resolve(config.workingDirectory, config.changelog))) {
+		filesToCommit.push(resolve(config.workingDirectory, config.changelog));
+	}
 	for (const file of files) {
-		filesToCommit.push(file.name);
+		filesToCommit.push(file.path);
 	}
 
 	// If there are no files to commit don't continue.
@@ -32,24 +37,34 @@ export async function commitChanges(
 		};
 	}
 
-	const gitAddOutput = await git("add", ...filesToCommit);
-
 	const shouldVerify = config.verify ? undefined : "--no-verify";
-	const shouldSign = config.sign ? "-S" : undefined;
-	const shouldCommitAll = config.commitAll ? [] : filesToCommit;
+	const shouldSign = config.sign ? "--gpg-sign" : undefined;
 
-	const gitCommitOutput = await git(
-		"commit",
-		shouldVerify,
-		shouldSign,
-		...shouldCommitAll,
-		"-m",
-		formatCommitMessage(config.changelogPresetConfig?.releaseCommitMessageFormat, nextVersion),
-	);
+	// If commitAll is set, commit all changed files.
+	if (config.commitAll) {
+		return {
+			filesToCommit,
+			gitAddOutput: await git("add", "--all"),
+			gitCommitOutput: await git(
+				"commit",
+				shouldVerify,
+				shouldSign,
+				"--message",
+				formatCommitMessage(config.changelogPresetConfig?.releaseCommitMessageFormat, nextVersion),
+			),
+		};
+	}
 
 	return {
 		filesToCommit,
-		gitAddOutput,
-		gitCommitOutput,
+		gitAddOutput: await git("add", ...filesToCommit),
+		gitCommitOutput: await git(
+			"commit",
+			shouldVerify,
+			shouldSign,
+			...filesToCommit,
+			"--message",
+			formatCommitMessage(config.changelogPresetConfig?.releaseCommitMessageFormat, nextVersion),
+		),
 	};
 }
