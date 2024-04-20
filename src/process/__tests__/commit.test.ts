ba84b4a2d471a543dbbf7c05dc1e478431d6f0c8
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2024-04-20 01:28:08 +0100

    test: combine test config + logger into createTestDir function

--- src/process/__tests__/commit.test.ts
+++ src/process/__tests__/commit.test.ts
@@ -1,5 +1,4 @@
 import { writeFileSync } from "node:fs";
-import { join } from "node:path";
 import { execFile } from "node:child_process";
 
 import { createTestDir } from "../../../tests/create-test-directory";
@@ -7,20 +6,18 @@ import { commitChanges } from "../commit";
 
 describe("commit", () => {
 	it("should commit changed files", async () => {
-		const { testDir, createTestConfig } = createTestDir("commit");
+		const { testFolder, relativeTo, config, logger } = await createTestDir("commit");
 
-		const { config, logger } = await createTestConfig();
-
-		writeFileSync(join(testDir, "CHANGELOG.md"), "Test content");
-		writeFileSync(join(testDir, "package.json"), JSON.stringify({ version: "1.2.3" }));
-		writeFileSync(join(testDir, "README.md"), "Test readme content");
+		writeFileSync(relativeTo("CHANGELOG.md"), "Test content");
+		writeFileSync(relativeTo("package.json"), JSON.stringify({ version: "1.2.3" }));
+		writeFileSync(relativeTo("README.md"), "Test readme content");
 
 		await commitChanges(
 			config,
 			logger,
 			[
-				{ name: "CHANGELOG.md", path: join(testDir, "CHANGELOG.md"), version: "1.2.4" },
-				{ name: "package.json", path: join(testDir, "package.json"), version: "1.2.4" },
+				{ name: "CHANGELOG.md", path: relativeTo("CHANGELOG.md"), version: "1.2.4" },
+				{ name: "package.json", path: relativeTo("package.json"), version: "1.2.4" },
 			],
 			"1.2.4",
 		);
@@ -29,18 +26,16 @@ describe("commit", () => {
 			"git",
 			["status", "--porcelain"],
 			{
-				cwd: testDir,
+				cwd: testFolder,
 			},
 			(error, stdout) => expect(stdout).toContain("?? README.md"),
 		);
 	});
 
 	it("should not commit if there are no files to commit", async () => {
-		const { testDir, createTestConfig } = createTestDir("commit");
+		const { testFolder, relativeTo, config, logger } = await createTestDir("commit");
 
-		const { config, logger } = await createTestConfig();
-
-		writeFileSync(join(testDir, "README.md"), "Test readme content");
+		writeFileSync(relativeTo("README.md"), "Test readme content");
 
 		await commitChanges(config, logger, [], "1.2.4");
 
@@ -48,28 +43,26 @@ describe("commit", () => {
 			"git",
 			["status", "--porcelain"],
 			{
-				cwd: testDir,
+				cwd: testFolder,
 			},
 			(error, stdout) => expect(stdout).toContain(""),
 		);
 	});
 
 	it("should commit all files if commitAll is set to true", async () => {
-		const { testDir, createTestConfig } = createTestDir("commit");
-
-		const { config, logger } = await createTestConfig();
+		const { testFolder, relativeTo, config, logger } = await createTestDir("commit");
 		config.commitAll = true;
 
-		writeFileSync(join(testDir, "CHANGELOG.md"), "Test content");
-		writeFileSync(join(testDir, "package.json"), JSON.stringify({ version: "1.2.3" }));
-		writeFileSync(join(testDir, "README.md"), "Test readme content");
+		writeFileSync(relativeTo("CHANGELOG.md"), "Test content");
+		writeFileSync(relativeTo("package.json"), JSON.stringify({ version: "1.2.3" }));
+		writeFileSync(relativeTo("README.md"), "Test readme content");
 
 		await commitChanges(
 			config,
 			logger,
 			[
-				{ name: "CHANGELOG.md", path: join(testDir, "CHANGELOG.md"), version: "1.2.4" },
-				{ name: "package.json", path: join(testDir, "package.json"), version: "1.2.4" },
+				{ name: "CHANGELOG.md", path: relativeTo("CHANGELOG.md"), version: "1.2.4" },
+				{ name: "package.json", path: relativeTo("package.json"), version: "1.2.4" },
 			],
 			"1.2.4",
 		);
@@ -78,7 +71,7 @@ describe("commit", () => {
 			"git",
 			["status", "--porcelain"],
 			{
-				cwd: testDir,
+				cwd: testFolder,
 			},
 			(error, stdout) => expect(stdout).toBe(""),
 		);
