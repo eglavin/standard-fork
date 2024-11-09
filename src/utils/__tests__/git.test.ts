9eceb33fde06649643ea6592b260c5241dbf31b0
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2024-11-09 01:03:31 +0000

    test: improve git class tests

--- src/utils/__tests__/git.test.ts
+++ src/utils/__tests__/git.test.ts
@@ -18,7 +18,22 @@ describe("git", () => {
 		expect(log).toMatch(/test: test arguments works/);
 	});
 
-	it("should not execute if dryRun is enabled", async () => {
+	it("should be able to tag commits", async () => {
+		const { testFolder, config, createCommits, createJSONFile } =
+			await createTestDir("execute-file");
+		const git = new Git(config);
+
+		createJSONFile();
+		createCommits();
+
+		await git.commit("--allow-empty", "-m", "test: test arguments works");
+		await git.tag("v1.0.0", "-m", "test: test arguments works");
+
+		const gitStatus = execSync("git status", { cwd: testFolder }).toString();
+		expect(gitStatus).toMatch(/nothing to commit, working tree clean/);
+	});
+
+	it("should not commit files if dryRun is enabled", async () => {
 		const { testFolder, config, createCommits, createJSONFile } =
 			await createTestDir("execute-file");
 		config.dryRun = true;
@@ -33,6 +48,36 @@ describe("git", () => {
 		expect(log).not.toMatch(/test: test arguments works/);
 	});
 
+	it("should not add files if dryRun is enabled", async () => {
+		const { testFolder, config, createCommits, createFile } = await createTestDir("execute-file");
+		config.dryRun = true;
+		const git = new Git(config);
+
+		createFile("\n", "file.txt");
+		createCommits();
+
+		await git.add("file.txt");
+		const gitStatus = execSync("git status", { cwd: testFolder }).toString();
+
+		expect(gitStatus).toMatch(/Untracked files:/);
+		expect(gitStatus).toMatch(/file.txt/);
+	});
+
+	it("should not tag commits if dryRun is enabled", async () => {
+		const { testFolder, config, createCommits, createJSONFile } =
+			await createTestDir("execute-file");
+		config.dryRun = true;
+		const git = new Git(config);
+
+		createJSONFile();
+		createCommits();
+
+		await git.commit("--allow-empty", "-m", "test: test arguments works");
+		await git.tag("v1.0.0", "-m", "test: test arguments works");
+
+		expect(() => execSync("git rev-list -n 1 v1.0.0", { cwd: testFolder }).toString()).toThrow();
+	});
+
 	it("should log if error is thrown", async () => {
 		const { config, createCommits, createJSONFile } = await createTestDir("execute-file");
 		const git = new Git(config);
@@ -43,6 +88,17 @@ describe("git", () => {
 		expect(async () => await git.add("non-existing-file")).rejects.toThrowError(
 			"Command failed: git add non-existing-file\nfatal: pathspec 'non-existing-file' did not match any files",
 		);
+	});
+
+	it("should be able to get the current branch", async () => {
+		const { config, createCommits, createJSONFile } = await createTestDir("execute-file");
+		const git = new Git(config);
+
+		createJSONFile();
+		createCommits();
+
+		const branch = await git.currentBranch();
+		expect(branch).toBe("main");
 	});
 
 	it("should check if a file is ignored by git", async () => {
