35be23ec865ea2850743fb295904bfd97191199d
Eanna Glavin <29385958+eglavin@users.noreply.github.com>
2024-11-10 22:01:53 +0000

    refactor: replace writeFileSync

--- src/process/__tests__/commit.test.ts
+++ src/process/__tests__/commit.test.ts
@@ -1,15 +1,13 @@
-import { writeFileSync } from "node:fs";
-
 import { setupTest } from "../../../tests/setup-tests";
 import { commitChanges } from "../commit";
 
 describe("commit", () => {
 	it("should commit changed files", async () => {
-		const { config, execGit, git, logger, relativeTo } = await setupTest("commit");
+		const { config, create, execGit, git, logger, relativeTo } = await setupTest("commit");
 
-		writeFileSync(relativeTo("CHANGELOG.md"), "Test content");
-		writeFileSync(relativeTo("package.json"), JSON.stringify({ version: "1.2.3" }));
-		writeFileSync(relativeTo("README.md"), "Test readme content");
+		create.file("Test content", "CHANGELOG.md");
+		create.json({ version: "1.2.3" }, "package.json");
+		create.file("Test readme content", "README.md");
 
 		await commitChanges(
 			config,
@@ -26,9 +24,9 @@ describe("commit", () => {
 	});
 
 	it("should not commit if there are no files to commit", async () => {
-		const { config, execGit, git, logger, relativeTo } = await setupTest("commit");
+		const { config, create, execGit, git, logger } = await setupTest("commit");
 
-		writeFileSync(relativeTo("README.md"), "Test readme content");
+		create.file("Test readme content", "README.md");
 
 		await commitChanges(config, logger, git, [], "1.2.4");
 
@@ -36,12 +34,12 @@ describe("commit", () => {
 	});
 
 	it("should commit all files if commitAll is set to true", async () => {
-		const { config, execGit, git, logger, relativeTo } = await setupTest("commit");
+		const { config, create, execGit, git, logger, relativeTo } = await setupTest("commit");
 		config.commitAll = true;
 
-		writeFileSync(relativeTo("CHANGELOG.md"), "Test content");
-		writeFileSync(relativeTo("package.json"), JSON.stringify({ version: "1.2.3" }));
-		writeFileSync(relativeTo("README.md"), "Test readme content");
+		create.file("Test content", "CHANGELOG.md");
+		create.json({ version: "1.2.3" }, "package.json");
+		create.file("Test readme content", "README.md");
 
 		await commitChanges(
 			config,
@@ -58,10 +56,10 @@ describe("commit", () => {
 	});
 
 	it("should skip commit", async () => {
-		const { config, execGit, git, logger, relativeTo } = await setupTest("commit");
+		const { config, create, execGit, git, logger, relativeTo } = await setupTest("commit");
 		config.skipCommit = true;
 
-		writeFileSync(relativeTo("package.json"), JSON.stringify({ version: "1.2.3" }));
+		create.json({ version: "1.2.3" }, "package.json");
 
 		await commitChanges(
 			config,
